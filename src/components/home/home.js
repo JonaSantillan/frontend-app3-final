f5533d47522117d2ee8c468b7133bbe6f672a811
Jonatan Santillan <jonatansantillan@gmail.com>
2021-03-16 11:36:21 -0300

    cv

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -13,6 +13,16 @@ class Productos extends React.Component {
       <div className={css.app}>
         <div className={css.container}>
           <button className={css.button} onClick={this.loginLogout}>{this.props.logged ? "Logout" : "Iniciar Sesión"}</button>        
+          <section className={css.listSection}>    
+          {!this.props.isFetchingProductos && !this.props.fail && <span className={css.listTitle}>Lista de Productos:</span>}
+          {this.props.isFetchingProductos && <span className={css.listTitle}>Cargando productos...</span>} 
+          {this.props.fail && <span className={css.listTitle}>Error al cargar productos...</span>}
+            {
+              this.props.productos.map((producto) => {
+                return <Producto history={this.props.history} key={producto._id} item={producto}/>
+              })
+            }
+          </section>
           <button hidden={!this.props.logged} className={css.button} onClick={this.addProducto}>Agregar Producto</button>
         </div>
       </div>
